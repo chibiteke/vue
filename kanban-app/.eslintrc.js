79a3b0b4e581eaf84fe121cfc6593ccdf9ef1780
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-01 10:12:55 +0900

    eslintの設定を見直し

--- kanban-app/.eslintrc.js
+++ kanban-app/.eslintrc.js
@@ -1,15 +1,31 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+    es6: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  extends: [
+    'eslint:recommended', // eslintの推奨ルール
+    // 'airbnb-base', // airbnbルール(厳しめ)
+    'plugin:prettier/recommended', // 1.eslint-plugin-prettierをenable
+    // 2.eslint-config-prettierをenable
+    // 3.prettierの整形に問題がある場合にerrorを出す設定
+    'plugin:vue/recommended', // eslint-plugin-vueを推奨設定でenable
+    'prettier/vue', // eslint-config-prettierのvue用ルールを適用(2019/1/16修正)
+  ],
+
   parserOptions: {
     parser: 'babel-eslint',
+    sourceType: 'module',
   },
+  plugins: ['vue'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-webpack-loader-syntax': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   overrides: [
     {
