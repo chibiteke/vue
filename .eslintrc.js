cba99f04a3a7e2634ab4c7d2ba24d2eb2d063670
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-03 22:07:24 +0900

    eslintの設定調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
   root: true,
-  parser: 'vue-eslint-parser', // parserをvue用に指定
-  parserOptions: {
-    parser: 'babel-eslint', // es6用のparserはoptionsの仲へ
-    sourceType: 'module',
-  },
   env: {
     browser: true,
     node: true,
+    es6: true,
   },
   extends: [
     'eslint:recommended', // eslintの推奨ルール
-    'airbnb-base', // airbnbルール(厳しめ)
+    // 'airbnb-base', // airbnbルール(厳しめ)
     'plugin:prettier/recommended', // 1.eslint-plugin-prettierをenable
     // 2.eslint-config-prettierをenable
     // 3.prettierの整形に問題がある場合にerrorを出す設定
@@ -19,32 +15,24 @@ module.exports = {
     'prettier/vue', // eslint-config-prettierのvue用ルールを適用(2019/1/16修正)
   ],
 
-  // add your custom type here
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
   rules: {
-    'no-var': 'error', //varを許可しない
-    'no-plusplus': 'off', // for文だけでなく、++は使いたかった
-    // 'func-names': 'off', // 無名関数を許可
-    // Vueだとスコープの問題で偶につかう
-    'vue/max-attributes-per-line': 'off',
-    'vue/component-name-in-template-casing': 'off',
-    // ↑ html部分をPascalにするかkebabにするかの選択です。
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always',
-        },
-      },
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        // semi: false,
-        singleQuote: true,
-      },
-    ],
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-webpack-loader-syntax': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  globals: {
-    Vue: true,
-  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
