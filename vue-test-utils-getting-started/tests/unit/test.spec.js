521d607b0fcbadf8142e9bafa2d6f9fcb417c2b9
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-06 01:00:53 +0900

    テストサンプル追加 https://vue-test-utils.vuejs.org/guides/#getting-started

--- vue-test-utils-getting-started/tests/unit/test.spec.js
+++ vue-test-utils-getting-started/tests/unit/test.spec.js
@@ -1,7 +1,9 @@
 // Import the mount() method from the test utils
 // and the component you want to test
-import { mount } from '@vue/test-utils';
+// import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import Counter from '@/components/counter';
+import Vue from 'vue';
 
 // wrapper.vmを介して実際のVueインスタンスにアクセスできます。
 // const { vm } = wrapper;
@@ -9,7 +11,7 @@ import Counter from '@/components/counter';
 
 describe('Counter', () => {
   // Now mount the component and you have the wrapper
-  const wrapper = mount(Counter);
+  const wrapper = shallowMount(Counter);
 
   it('renders the correct markup', () => {
     expect(wrapper.html()).toContain('<span class="count">0</span>');
@@ -26,4 +28,32 @@ describe('Counter', () => {
     button.trigger('click');
     expect(wrapper.vm.count).toBe(1);
   });
+
+  //   // エラーは捕捉されません
+  //   it('will time out', (done) => {
+  //     Vue.nextTick(() => {
+  //       expect(true).toBe(false);
+  //       done();
+  //     });
+  //   });
+
+  //   // 以下の3つのテストは期待通りに動作します。
+  //   it('will catch the error using done', (done) => {
+  //     Vue.config.errorHandler = done;
+  //     Vue.nextTick(() => {
+  //       expect(true).toBe(false);
+  //       done();
+  //     });
+  //   });
+
+  //   it('will catch the error using a promise', () => {
+  //     return Vue.nextTick().then(function () {
+  //       expect(true).toBe(false);
+  //     });
+  //   });
+
+  //   it('will catch the error using async/await', async () => {
+  //     await Vue.nextTick();
+  //     expect(true).toBe(false);
+  //   });
 });
